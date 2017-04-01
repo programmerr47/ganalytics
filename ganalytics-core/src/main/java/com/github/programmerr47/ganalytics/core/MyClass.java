9eb0937977d7907cfd883136953d5cf3b2f4f497
programmerr47 <m.spitsin@rambler-co.ru>
2017-04-01 23:15:43 +0300

    init core library project

---
+++ ganalytics-core/src/main/java/com/github/programmerr47/ganalytics/core/MyClass.java
@@ -0,0 +1,13 @@
+package com.github.programmerr47.ganalytics.core;
+
+public class MyClass {
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
