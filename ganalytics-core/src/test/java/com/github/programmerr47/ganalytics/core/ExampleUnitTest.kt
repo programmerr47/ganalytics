7d169ccd2b23460ab4d8dbe513f73a875e1bfef4
programmerr47 <m.spitsin@rambler-co.ru>
2017-04-01 23:35:15 +0300

    set up unit tests for project

---
+++ ganalytics-core/src/test/java/com/github/programmerr47/ganalytics/core/ExampleUnitTest.kt
@@ -0,0 +1,9 @@
+package com.github.programmerr47.ganalytics.core
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun exampleCheck() = assertEquals(45, MyClass(45).state)
+}
